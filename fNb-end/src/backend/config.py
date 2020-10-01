dd28acfe942e16282ddfb2aba1631b86421fab5d
kauereblin <kaue.reblin159@gmail.com>
2020-10-01 18:11:31 -0300

    add cors feature

--- fNb-end/src/backend/config.py
+++ fNb-end/src/backend/config.py
@@ -1,10 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 from database.connection import db_file_path
 
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_file_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
