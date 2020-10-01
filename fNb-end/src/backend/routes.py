da92df1fea7562afbc130ef4e3f95e454d9514f7
kauereblin <kaue.reblin159@gmail.com>
2020-10-01 18:14:14 -0300

    add post route to include data

--- fNb-end/src/backend/routes.py
+++ fNb-end/src/backend/routes.py
@@ -1,20 +1,36 @@
-from config import app, db, jsonify
+from config import app, db, jsonify, request
 from models.HelicopteroDeCombate import HelicopteroDeCombate
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return '<h1>Testando as paradas por aqui</h1><br /><a href="index_helicopters">Clica aí para ver se o bd tá funcionando</a>'
 
 
-@app.route('/index_helicopters')
+@app.route('/index_helicopters',  methods=['GET'])
 def index_helicopters():
     helicopters = db.session.query(HelicopteroDeCombate).all()
 
     json_helis_list = [helicopter.json() for helicopter in helicopters]
 
     response = jsonify(json_helis_list)
+    response.headers.add("Access-Control-Allow-Origin", "*")
 
+    return response
+
+@app.route('/create_helicopter',  methods=['POST'])
+def create_helicopter():
+    response = jsonify({ "result": "ok", "details": "ok" })
+
+    data = request.get_json()
+
+    try:
+        new_helicopter = HelicopteroDeCombate(**data)
+        db.session.add(new_helicopter)
+        db.session.commit()
+    except Exception as e:
+            response = jsonify({ "result": "error", "details": str(e) })
+    
     response.headers.add("Access-Control-Allow-Origin", "*")
 
     return response
