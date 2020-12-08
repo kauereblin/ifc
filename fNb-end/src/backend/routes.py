a1ed96370c3ba97e6f5a166c8ec4540e01f1207b
kauereblin <kaue.reblin159@gmail.com>
2020-12-08 17:38:56 -0300

    update index route

--- fNb-end/src/backend/routes.py
+++ fNb-end/src/backend/routes.py
@@ -1,24 +1,38 @@
 from config import app, db, jsonify, request
 from models.HelicopteroDeCombate import HelicopteroDeCombate
+from models.Pilot import Pilot
+from models.Hangar import Hangar
 
 
 @app.route('/', methods=['GET'])
 def index():
-    return '<h1>Testando as paradas por aqui</h1><br /><a href="index_helicopters">Clica aí para ver se o bd tá funcionando</a>'
+    return '''<h1>Testando as paradas por aqui</h1>
+            <br />
+            <a href="index_helicopters">
+                Clica aí para ver se o bd tá funcionando
+            </a>'''
 
 
-@app.route('/index_helicopters',  methods=['GET'])
-def index_helicopters():
-    helicopters = db.session.query(HelicopteroDeCombate).all()
+@app.route('/index/<string:table>')
+def index_table(table):
+    data = None
 
-    json_helis_list = [helicopter.json() for helicopter in helicopters]
+    if table == "helicoptero-de-combates":
+        data = db.session.query(HelicopteroDeCombate).all()
+    elif table == "pilots":
+        data = db.session.query(Pilot).all()
+    elif table == "hangars":
+        data = db.session.query(Hangar).all()
 
-    response = jsonify(json_helis_list)
+    json_list = [ _.json() for _ in data ]
+
+    response = jsonify(json_list)
     response.headers.add("Access-Control-Allow-Origin", "*")
 
     return response
 
-@app.route('/create_helicopter',  methods=['POST'])
+
+@app.route('/create_helicopter', methods=['POST'])
 def create_helicopter():
     response = jsonify({ "result": "ok", "details": "ok" })
 
@@ -34,6 +48,7 @@ def create_helicopter():
     response.headers.add("Access-Control-Allow-Origin", "*")
 
     return response
+
 
 @app.route('/delete_helicopter/<int:helicopter_id>', methods=['DELETE'])
 def delete_helicopter(helicopter_id):
